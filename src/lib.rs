4dd778f9de08f1cf1169bf6ccef173918b65f8d2
Nandan-Tav27 <nandantavargeri@gmail.com>
2026-03-24 12:53:28 +0530

    Communicate with channels

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::future::Future;
-use std::sync::{Arc, Condvar, Mutex};
 use std::collections::{HashMap, VecDeque};
-use std::task::{Context, Poll, Wake, Waker};
+use std::future::Future;
 use std::pin::Pin;
+use std::sync::{Arc, Condvar, Mutex};
+use std::task::{Context, Poll, Wake, Waker};
 
 #[derive(Copy, Clone, Hash, PartialEq, Eq)]
 struct TaskId(usize);
@@ -17,17 +17,9 @@ struct TaskWaker {
     ready_queue: Arc<ReadyQueue>,
 }
 
-struct Counter {
-    current: u32,
-    max: u32,
-}
-
 impl TaskWaker {
     fn new(id: TaskId, ready_queue: Arc<ReadyQueue>) -> Self {
-        TaskWaker {
-            id,
-            ready_queue,
-        }
+        TaskWaker { id, ready_queue }
     }
 }
 
@@ -41,17 +33,15 @@ impl Executor {
     pub fn new() -> Self {
         Executor {
             tasks: HashMap::new(),
-            ready_queue: Arc::new(
-                    ReadyQueue {
-                    queue: Mutex::new(VecDeque::new()),
-                    condvar: Condvar::new(),
-                }
-            ),
+            ready_queue: Arc::new(ReadyQueue {
+                queue: Mutex::new(VecDeque::new()),
+                condvar: Condvar::new(),
+            }),
             next_id: 1,
         }
     }
 
-    pub fn spawn<F: Future<Output = ()> + 'static>(&mut self, future: F)  {
+    pub fn spawn<F: Future<Output = ()> + 'static>(&mut self, future: F) {
         let task_id = TaskId(self.next_id);
         self.next_id += 1;
         self.tasks.insert(task_id, Box::pin(future));
@@ -73,43 +63,19 @@ impl Executor {
             while let Some(task_id) = queue.pop_front() {
                 drop(queue);
 
-                let waker: Waker = Arc::new(TaskWaker::new(task_id, self.ready_queue.clone())).into();
+                let waker: Waker =
+                    Arc::new(TaskWaker::new(task_id, self.ready_queue.clone())).into();
                 let mut cx = Context::from_waker(&waker);
 
                 let task = self.tasks.get_mut(&task_id).unwrap();
                 match task.as_mut().poll(&mut cx) {
-                    Poll::Pending => {
-                    },
+                    Poll::Pending => {}
                     Poll::Ready(_) => {
                         self.tasks.remove(&task_id);
-                    },
+                    }
                 }
                 queue = self.ready_queue.queue.lock().unwrap();
             }
-        }
-    }
-}
-
-impl Future for Counter {
-    type Output = ();
- 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
-        self.current += 1;
-        println!("Count: {}", self.current);
-        if self.current < self.max {
-            cx.waker().wake_by_ref();
-            Poll::Pending
-        } else {
-            Poll::Ready(())
-        }
-    }
-}
-
-impl Counter {
-    fn new(max: u32) -> Self {
-        Counter {
-            current: 0,
-            max,
         }
     }
 }
@@ -121,17 +87,74 @@ impl Wake for TaskWaker {
     }
 }
 
+struct Inner<T> {
+    queue: VecDeque<T>,
+    waker: Option<Waker>,
+}
+
+pub struct Sender<T> {
+    inner: Arc<Mutex<Inner<T>>>,
+}
+
+pub struct Receiver<T> {
+    inner: Arc<Mutex<Inner<T>>>,
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let inner = Arc::new(Mutex::new(Inner {
+        queue: VecDeque::new(),
+        waker: None,
+    }));
+    (
+        Sender {
+            inner: inner.clone(),
+        },
+        Receiver { inner },
+    )
+}
+
+impl<T> Sender<T> {
+    pub fn send(&self, message: T) {
+        let mut inner = self.inner.lock().unwrap();
+        inner.queue.push_back(message);
+        if let Some(waker) = inner.waker.take() {
+            waker.wake();
+        }
+    }
+}
+
+impl<T: 'static> Future for Receiver<T> {
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<T> {
+        let mut inner = self.inner.lock().unwrap();
+        if let Some(message) = inner.queue.pop_front() {
+            Poll::Ready(message)
+        } else {
+            inner.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_executor() {
+    fn test_channel() {
         let mut executor = Executor::new();
+        let (tx, rx) = channel::<u32>();
 
-        executor.spawn(Counter::new(3));
-        executor.spawn(Counter::new(3));
+        executor.spawn(async move {
+            tx.send(42);
+        });
+
+        executor.spawn(async move {
+            let val = rx.await;
+            assert_eq!(val, 42);
+        });
+
         executor.run();
     }
 }
-
